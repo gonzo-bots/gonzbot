393bde70623ee455f5ffd3a6c0393ea44cf09069
BrandonWester <brandonjwester@gmail.com>
2023-04-22 10:53:21 -0400

    initial commit

---
+++ gonzobot.py
@@ -0,0 +1,41 @@
+import discord
+import os
+from dotenv import load_dotenv
+from discord.ext import commands
+
+
+load_dotenv()
+
+discordtoken = os.getenv('token')
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("pee")
+
+@bot.command()
+async def add(ctx, a: int, b: int):
+    await ctx.send(a + b)
+
+
+
+'''@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+    if message.content.startswith('why does this stop this from working?'):
+        await message.channel.send("cause you're dumb")'''
+
+@bot.command()
+async def kick(ctx, member: commands.MemberConverter):
+    await ctx.guild.kick(member)
+    await ctx.send(f'{member} has been kicked.')
+
+
+bot.run(discordtoken)
