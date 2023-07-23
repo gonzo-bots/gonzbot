e6314c47ad51b590a31a39f00a0bdc429aba30ce
tea-freaking-reks <130566245+tea-freaking-reks@users.noreply.github.com>
2023-07-23 19:46:40 -0400

    Update gonzobot.py

--- gonzobot.py
+++ gonzobot.py
@@ -5,7 +5,7 @@ from discord.ext import commands
 
 #comment for test
 
-discordtoken =str(os.getenv('env.PYTOKEN'))
+discordtoken =str(os.getenv('PYTOKEN'))
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix='!', intents=intents)
 
