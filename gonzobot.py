5c4451bc841ca7b1caccea860fcf6e30f815f426
BrandonWester <brandonjwester@gmail.com>
2023-07-24 13:24:15 -0400

    undoing token

--- gonzobot.py
+++ gonzobot.py
@@ -3,9 +3,9 @@ import os
 from discord.ext import commands
 
 
-#str(os.getenv('PYTOKEN'))
 
-discordtoken = 'TVRBNU9USXdNelUxTXpJeU9ESTBNamszTkEuR1hPWklDLnFZa2pKUlFmUkRLYjh5QW9iQUg0d1QybUw3SU5NY1B6U1BTMUJJ'
+
+discordtoken = str(os.getenv('PYTOKEN'))
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix='!', intents=intents)
 
