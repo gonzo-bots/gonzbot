85b8c31bc06b9d0b463378f5b97896286e17ca52
BrandonWester <brandonjwester@gmail.com>
2023-07-30 18:47:51 -0400

    print pytoken

--- gonzobot.py
+++ gonzobot.py
@@ -3,6 +3,7 @@ import os
 from discord.ext import commands
 
 discordtoken = os.getenv('pytoken')
+print(os.getenv('pytoken'))
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix='!', intents=intents)
 
