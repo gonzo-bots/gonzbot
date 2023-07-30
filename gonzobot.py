cef5020dba0b483ea0d8449e283ee9f97dbf12d2
BrandonWester <brandonjwester@gmail.com>
2023-07-30 19:38:27 -0400

    discord print change

--- gonzobot.py
+++ gonzobot.py
@@ -4,6 +4,7 @@ from discord.ext import commands
 
 discordtoken = os.getenv('pytoken')
 print(os.getenv('pytoken'))
+print(str(os.getenv('pytoken')))
 intents = discord.Intents.default()
 bot = commands.Bot(command_prefix='!', intents=intents)
 
