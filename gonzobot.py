a9e183c147a08f8aacd1f5f7f4d7358943a71a02
BrandonWester <brandonjwester@gmail.com>
2023-08-01 15:29:53 -0400

    json lib cleanup

--- gonzobot.py
+++ gonzobot.py
@@ -7,7 +7,7 @@ token_json = os.getenv('pytoken')
 token_dict = json.loads(token_json)
 for k, v in token_dict.items():
     discord_token = v
-print(discord_token)
+
 
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix='!', intents=intents)
